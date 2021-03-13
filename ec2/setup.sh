770d58c9924904e1de2ede3af8b9a209c92b9e04
Chris Navta <christophernavta@hobby-hole-mac.lan>
2021-03-13 15:28:03 -0500

    Faster!

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -4,6 +4,6 @@ sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo systemctl enable containerd.service
 sudo systemctl start containerd.service
-sleep 30
+sleep 10
 sudo docker run -d -it -e EULA=TRUE -e WHITE_LIST_USERS="Gonj,Paper2412,Starlight1814" -p 19132:19132/udp itzg/minecraft-bedrock-server
-sleep 30
+sleep 10
