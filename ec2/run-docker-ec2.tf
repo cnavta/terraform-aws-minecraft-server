01f9049b1593b6db6766b5e3300a667c85ad1ae1
Chris Navta <christophernavta@hobby-hole-mac.lan>
2021-03-13 02:14:56 -0500

    Update run-docker-ec2.tf
    De'Kratta

--- ec2/run-docker-ec2.tf
+++ ec2/run-docker-ec2.tf
@@ -50,11 +50,6 @@ resource "aws_eip" "minecraft-ip" {
 
 }
 
-resource "aws_ebs_volume" "minecraft-storage" {
-  availability_zone = "us-west-2a"
-  size = 5
-}
-
 resource "aws_instance" "minecraftr-ec2" {
   ami = "ami-00f9f4069d04c0c6e"
   instance_type = "t2.micro"
@@ -91,14 +86,8 @@ resource "aws_instance" "minecraftr-ec2" {
 resource "aws_eip_association" "minecraft-ip-assoc" {
   instance_id = aws_instance.minecraftr-ec2.id
   network_interface_id = aws_eip.minecraft-ip.network_interface
+  public_ip = aws_eip.minecraft-ip.public_ip
 }
-
-resource "aws_volume_attachment" "minecraft-storage-att" {
-  device_name = "/data"
-  volume_id   = aws_ebs_volume.minecraft-storage.id
-  instance_id = aws_instance.minecraftr-ec2.id
-}
-
 
 output "external-ip" {
   value = aws_eip.minecraft-ip.public_ip
