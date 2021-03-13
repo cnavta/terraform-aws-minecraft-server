49655325786455421ef4840ae3da99d3b7d4112f
Chris Navta <christophernavta@hobby-hole-mac.lan>
2021-03-13 01:50:37 -0500

    Start 'er up!

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo yum -y install docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
 docker run -d -it -e EULA=TRUE -p 19132:19132/udp itzg/minecraft-bedrock-server
