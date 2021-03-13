009592ca052ff919bf20d04dc95d3e4e6dcd0bde
Chris Navta <christophernavta@hobby-hole-mac.lan>
2021-03-13 13:19:21 -0500

    Whitelist

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -5,5 +5,5 @@ sudo systemctl start docker.service
 sudo systemctl enable containerd.service
 sudo systemctl start containerd.service
 sleep 30
-sudo docker run -d -it -e EULA=TRUE -p 19132:19132/udp itzg/minecraft-bedrock-server
+sudo docker run -d -it -e EULA=TRUE -e WHITE_LIST_USERS="Gonj,Paper2412,Starlight1814" -p 19132:19132/udp itzg/minecraft-bedrock-server
 sleep 30
