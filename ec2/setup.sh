d500d6e0ccd5612ae5407d2cb36005c37f2abe44
Chris Navta <christophernavta@hobby-hole-mac.lan>
2021-03-13 12:18:12 -0500

    So tired . . .

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -4,4 +4,6 @@ sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo systemctl enable containerd.service
 sudo systemctl start containerd.service
+sleep 30
 sudo docker run -d -it -e EULA=TRUE -p 19132:19132/udp itzg/minecraft-bedrock-server
+sleep 30
